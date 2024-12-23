58f3f80200bdab3ccb1744c9ddeab10a7052189f
wvandeneede <193086+wvandeneede@users.noreply.github.com>
2024-12-23 12:49:10 +0100

    copy game loop and setup testing

--- codingame.winter2024.game/Program.cs
+++ codingame.winter2024.game/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Grow and multiply your organisms to end up larger than your opponent.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int width = int.Parse(inputs[0]); // columns in the game grid
+        int height = int.Parse(inputs[1]); // rows in the game grid
+
+        // game loop
+        while (true)
+        {
+            int entityCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < entityCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int x = int.Parse(inputs[0]);
+                int y = int.Parse(inputs[1]); // grid coordinate
+                string type = inputs[2]; // WALL, ROOT, BASIC, TENTACLE, HARVESTER, SPORER, A, B, C, D
+                int owner = int.Parse(inputs[3]); // 1 if your organ, 0 if enemy organ, -1 if neither
+                int organId = int.Parse(inputs[4]); // id of this entity if it's an organ, 0 otherwise
+                string organDir = inputs[5]; // N,E,S,W or X if not an organ
+                int organParentId = int.Parse(inputs[6]);
+                int organRootId = int.Parse(inputs[7]);
+            }
+            inputs = Console.ReadLine().Split(' ');
+            int myA = int.Parse(inputs[0]);
+            int myB = int.Parse(inputs[1]);
+            int myC = int.Parse(inputs[2]);
+            int myD = int.Parse(inputs[3]); // your protein stock
+            inputs = Console.ReadLine().Split(' ');
+            int oppA = int.Parse(inputs[0]);
+            int oppB = int.Parse(inputs[1]);
+            int oppC = int.Parse(inputs[2]);
+            int oppD = int.Parse(inputs[3]); // opponent's protein stock
+            int requiredActionsCount = int.Parse(Console.ReadLine()); // your number of organisms, output an action for each one in any order
+            for (int i = 0; i < requiredActionsCount; i++)
+            {
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+                Console.WriteLine("WAIT");
+            }
+        }
+    }
+}
--- codingame.winter2024.test/UnitTest1.cs
+++ codingame.winter2024.test/UnitTest1.cs
@@ -5,6 +5,9 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var p = new Point(0, 0);
 
+        Assert.Equal(0, p.X);
+        Assert.Equal(0, p.Y);
     }
 }
